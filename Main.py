a20e864e43fbf49c30c21f097c476c6764a64d90
Sinan İbrahim Bayraktar <_sinan_@windowslive.com>
2017-06-17 01:00:36 +0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,15 @@
+import numpy
+array1 = numpy.array([1, 2, 3])
+array2 = numpy.array([4, 5, 6])
+sum = array1 + array2
+mult = array1 * array2
+print (sum)
+print (mult)
+array3 = numpy.array ([2, 2, 2])
+exp = sum ** array3
+print (exp)
+someList = [22, 33, 55]
+array4 = numpy.array(someList)
+print (array4)
+x = input()
+
