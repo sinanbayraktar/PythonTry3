4d5deeb135be0da857de6c8d5a3512f82002e3e1
Sinan İbrahim Bayraktar <_sinan_@windowslive.com>
2017-06-17 01:10:27 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,15 +1,16 @@
 import numpy
 array1 = numpy.array([1, 2, 3])
 array2 = numpy.array([4, 5, 6])
-sum = array1 + array2
-mult = array1 * array2
+sum = array1 + array2  # summation
+mult = array1 * array2  # multiplication
 print (sum)
 print (mult)
-array3 = numpy.array ([2, 2, 2])
-exp = sum ** array3
+array3 = numpy.array ([4, 3, 2])
+exp = sum ** array3  ## taking explonential according to an array
 print (exp)
+exp2 = sum **2  # taking square
+print (exp2)
 someList = [22, 33, 55]
 array4 = numpy.array(someList)
 print (array4)
 x = input()
-
